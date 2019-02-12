d049cf5c042e3ca89c51d668e4242fdb78ce19c5
puremdq <puremdq@gmail.com>
2019-02-12 18:09:45 +0800

    web socket

---
+++ admin/src/main/java/com/aojiaoo/core/socket/MyHandler.java
@@ -0,0 +1,15 @@
+package com.aojiaoo.core.socket;
+
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+public class MyHandler extends TextWebSocketHandler {
+
+    @Override
+    public void handleTextMessage(WebSocketSession session, TextMessage message) {
+        System.out.println(message);
+        // ...
+    }
+
+}
---
+++ admin/src/main/java/com/aojiaoo/modules/kissPlan/controller/GreetingController.java
@@ -0,0 +1,19 @@
+package com.aojiaoo.modules.kissPlan.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class GreetingController {
+
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public String greeting(String message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return "message";
+    }
+
+}
