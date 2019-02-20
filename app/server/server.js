4775d175bf227948cbdbc04cbbf1a72326ea7ab6
puremdq <puremdq@gmail.com>
2019-02-20 23:35:10 +0800

    Update server.js

--- app/server/server.js
+++ app/server/server.js
@@ -85,7 +85,7 @@ app.get('*', (req, res) => {
     })
 })
   
-const port = process.env.PORT || 3000
+const port = /*process.env.PORT ||*/ 3000
 app.listen(port, () => {
   console.log(`server started at http://localhost:${port}`)
 })
