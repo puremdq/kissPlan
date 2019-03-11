a7b74825d5445c7a724aca989c049bbbab22fd90
puremdq <puremdq@gmail.com>
2019-03-11 18:51:50 +0800

    日期转换

---
+++ admin/src/main/java/com/aojiaoo/config/converter/SpringMVCConfig.java
@@ -0,0 +1,17 @@
+//package com.aojiaoo.config.converter;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.format.FormatterRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+//
+//@Configuration
+//public class SpringMVCConfig extends WebMvcConfigurationSupport {
+//
+//    /**
+//     * 添加自定义的Converters和Formatters.
+//     */
+//    @Override
+//    protected void addFormatters(FormatterRegistry registry) {
+//        registry.addConverter(new StringToDateConverter());
+//    }
+//}
---
+++ admin/src/main/java/com/aojiaoo/config/converter/StringToDateConverter.java
@@ -0,0 +1,17 @@
+//package com.aojiaoo.config.converter;
+//
+//import com.aojiaoo.utils.DateUtils;
+//import org.springframework.core.convert.converter.Converter;
+//
+//import java.util.Date;
+//
+//public class StringToDateConverter implements Converter<String, Date> {
+//
+//    @Override
+//    public Date convert(String value) {
+//
+//        return DateUtils.parseDate(value);
+//    }
+//
+//
+//}
